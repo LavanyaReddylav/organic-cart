855a8142ccf8e80293d9941bf030c7716b69ded8
lavanya y <ylavanyappp@gmail.com>
2020-03-11 13:29:04 +0530

    .

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/organic-app'));
+app.use(express.static(__dirname + '/dist/organic-cart'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/organic-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/organic-cart/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
